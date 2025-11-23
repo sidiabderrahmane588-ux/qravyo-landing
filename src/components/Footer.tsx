9b0248e24c2cc5b9259e7901e55ea2805c433876
sidiabderrahmane588-ux <sidiabderrahmane588@gmail.com>
2025-11-24 00:07:47 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { FileText, Instagram, Facebook, Twitter } from 'lucide-react';
+import { FileText, Instagram, Facebook, Twitter, Mail } from 'lucide-react';
 import LegalModal from './LegalModal';
 import PrivacyModal from './PrivacyModal';
 import RefundModal from './RefundModal';
@@ -33,8 +33,20 @@ const Footer = () => {
             </p>
           </div>
 
+          {/* Contact */}
+          <div className="space-y-4">
+            <h4 className="text-lg font-semibold">Contact</h4>
+            <a
+              href="mailto:qravyo@gmail.com"
+              className="flex items-center space-x-2 text-gray-400 hover:text-white
+                        transition-colors duration-200">
+              <Mail size={18} />
+              <span>qravyo@gmail.com</span>
+            </a>
+          </div>
+
           {/* Informations */}
-          <div className="space-y-4 md:col-span-2">
+          <div className="space-y-4">
             <h4 className="text-lg font-semibold">Informations</h4>
             <div className="space-y-3">
               <button
