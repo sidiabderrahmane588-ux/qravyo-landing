3f170b2362d331bbcdea09bd166e7da4f28fa6f0
sidiabderrahmane588-ux <sidiabderrahmane588@gmail.com>
2025-11-24 19:13:31 +0100

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-gray-900 text-white">
+    <footer className="bg-blue-600 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
         
         {/* Main Footer Content */}
@@ -96,7 +96,7 @@ const Footer = () => {
         </div>
 
         {/* Social Media Icons */}
-        <div className="border-t border-gray-800 pt-8 pb-6">
+        <div className="border-t border-blue-500 pt-8 pb-6">
           <div className="flex justify-center items-center space-x-6">
             <a
               href="https://instagram.com/qravyo"
@@ -146,7 +146,7 @@ const Footer = () => {
         </div>
 
         {/* Bottom Bar */}
-        <div className="border-t border-gray-800 pt-8">
+        <div className="border-t border-blue-500 pt-8">
           <div className="flex justify-center items-center">
             <p className="text-gray-400 text-sm">
               © {currentYear} QRAVYO. Tous droits réservés.
