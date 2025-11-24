81dffd18511651e11ac29d4c23e8de78f5ed5cc3
sidiabderrahmane588-ux <sidiabderrahmane588@gmail.com>
2025-11-24 17:27:04 +0100

    Updated Benefits.tsx

--- src/components/Benefits.tsx
+++ src/components/Benefits.tsx
@@ -10,7 +10,7 @@ const benefits = [
   },
   {
     icon: Heart,
-    title: "Fidélisation client",
+    title: "Fidélisation cliente",
     description: "Créez un lien unique avec vos clients grâce à l'expérience ludique",
     gradient: "from-red-500 to-pink-500"
   },
